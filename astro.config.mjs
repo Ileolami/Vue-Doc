07e513a27186f0da906aae6aa1e38389d0262818
Ileolami <99335756+Ileolami@users.noreply.github.com>
2025-04-20 22:00:47 +0100

    chore: update dependencies and add Tailwind CSS support
    - Added @tailwindcss/vite, autoprefixer, and postcss to dependencies and devDependencies in package.json.
    - Updated example.mdx to include a link to Starlight's reference.
    - Created a new quick start guide for Vue in quick.mdx.
    - Enhanced the index.mdx page with improved descriptions and card layout.
    - Removed custom.css and replaced it with a new styles/custom.css file containing updated styles and dark/light mode support.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,61 +6,61 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		react(),
-		starlight({
-			title: 'Vue.js',
-			customCss: [
-				// Relative path to your custom CSS file
-				'./src/style/custom.css',
-			  ],
-			favicon: './public/logo.svg',
-			logo: {
-				src: './public/logo.svg',
-			},
-			social: [
-				{ icon: 'github', label: 'GitHub', href: 'https://github.com/vuejs/' },
-				{ icon: 'twitter', label: 'Twitter', href: 'https://twitter.com/vuejs' },
-				{ icon: 'discord', label: 'Discord', href: 'https://discord.gg/vuejs' }
-			],
-			sidebar: [
-				{
-					label: 'Getting Started',
-					autogenerate: { directory: 'guides' },
-				},
-				{
-					label: 'Essentials',
-					autogenerate: { directory: 'essentials' },
-				},
-				{
-					label: 'Components In-Depth',
-					autogenerate: { directory: 'components'}
-				},
-				{
-					label: 'Reusability',
-					autogenerate: { directory: 'reusability'}
-				},
-				{
-					label: 'Built-in Components',
-					autogenerate: { directory: 'built-in'}
-				},
-				{
-					label: 'Scaling Up',
-					autogenerate: { directory: 'scaling-up'}
-				},
-				{
-					label: 'Best Practices',
-					autogenerate: { directory: 'best-practices'}
-				},
-				{
-					label: 'Typescript',
-					autogenerate: { directory: 'typescript'}
-				},
-				{
-					label: 'Extra Topics',
-					autogenerate: { directory: 'extra-topics'}
-				}
-			],
-		}),
+  integrations: [
+      react(),
+      starlight({
+          title: 'Vue.js',
+          customCss: [
+              // Relative path to your custom CSS file
+			  './src/styles/custom.css'
+            ],
+          favicon: './public/logo.svg',
+          logo: {
+              src: './public/logo.svg',
+          },
+          social: [
+              { icon: 'github', label: 'GitHub', href: 'https://github.com/vuejs/' },
+              { icon: 'twitter', label: 'Twitter', href: 'https://twitter.com/vuejs' },
+              { icon: 'discord', label: 'Discord', href: 'https://discord.gg/vuejs' }
+          ],
+          sidebar: [
+              {
+                  label: 'Getting Started',
+                  autogenerate: { directory: 'guides' },
+              },
+              {
+                  label: 'Essentials',
+                  autogenerate: { directory: 'essentials' },
+              },
+              {
+                  label: 'Components In-Depth',
+                  autogenerate: { directory: 'components'}
+              },
+              {
+                  label: 'Reusability',
+                  autogenerate: { directory: 'reusability'}
+              },
+              {
+                  label: 'Built-in Components',
+                  autogenerate: { directory: 'built-in'}
+              },
+              {
+                  label: 'Scaling Up',
+                  autogenerate: { directory: 'scaling-up'}
+              },
+              {
+                  label: 'Best Practices',
+                  autogenerate: { directory: 'best-practices'}
+              },
+              {
+                  label: 'Typescript',
+                  autogenerate: { directory: 'typescript'}
+              },
+              {
+                  label: 'Extra Topics',
+                  autogenerate: { directory: 'extra-topics'}
+              }
+          ],
+      }),
 	],
 });
